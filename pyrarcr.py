bfe697800f620191355f5520a7c35e0320e1d2d2
b40153 <Jonatan_Dellagostin@banrisul.com.br>
2015-09-29 11:32:32 -0300

    Adding support for any character and for customized alphabet

--- pyrarcr.py
+++ pyrarcr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 #####      ##### ##### #####             ####   ####   #
 #   # #  # #   # #   # #   # #### ####   #  #      #  ##
@@ -7,11 +7,9 @@
 #       #  #   # #   # #   # #### #      #### # #### # #
 
 #finds the password of a desired rar or zip file using a brute-force algorithm
-##will fail to find the password if the password has a character that isnt in
-##the english alphabet or isnt a number (you can change the char. list though)
 
 #importing needed modules
-import time,os,sys,shutil,itertools
+import time,os,sys,shutil,itertools, re
 
 name=os.path.basename(__file__)
 
@@ -26,17 +24,25 @@ for which in ["unrar","p7zip"]:
   sys.exit(-1)
 
 #defining the function
-def rc(rf):
- alphabet="aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890"
+def rc(rf, alphabet):
  start=time.time()
  tryn=0
+ counterTmp = 0
+ printCounter = 1000
  for a in range(1,len(alphabet)+1):
   for b in itertools.product(alphabet,repeat=a):
    k="".join(b)
+   k=re.escape(k)
    if rf[-4:]==".rar":
-    print("Trying:",k)
-    kf=os.popen("unrar t -y -p%s %s 2>&1|grep 'All OK'"%(k,rf))
+    cmd = "unrar t -y -p%s %s 2>&1|grep 'All OK'"%(k,rf)
+    kf=os.popen(cmd)
     tryn+=1
+    counterTmp+=1
+    if counterTmp >= printCounter:
+      print ('Trying combination number '+str(tryn)+':'+str(k))
+      timeWasted = round(time.time()-start,2)
+      print("It took already ",timeWasted,"seconds. Speed: ",round(tryn/timeWasted,2)," passwords/sec")
+      counterTmp=0
     for rkf in kf.readlines():
      if rkf=="All OK\n":
       print("Found password:",repr(k))
@@ -46,9 +52,14 @@ def rc(rf):
       time.sleep(2)
       sys.exit(1)
    elif rf[-4:]==".zip" or rf[-3:]==".7z":
-    print("Trying:",k)
     kf=os.popen("7za t -p%s %s 2>&1|grep 'Everything is Ok'"%(k,rf))
     tryn+=1
+    counterTmp+=1
+    if counterTmp >= printCounter:
+      print ('Trying combination number '+str(tryn)+':'+str(k))
+      timeWasted = round(time.time()-start,2)
+      print("It took already ",timeWasted,"seconds. Speed: ",round(tryn/timeWasted,2)," passwords/sec")
+      counterTmp=0
     for rkf in kf.readlines():
      if rkf=="Everything is Ok\n":
       print("Found password:",repr(k))
@@ -60,12 +71,25 @@ def rc(rf):
    else:
     print("ERROR: File isnt a RAR, ZIP or 7z file.\nExiting...")
 
+defaultAlphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890"
+name = sys.argv[0]
+helpMessage = '''Usage:    '''+name+''' [rar file]
+          '''+name+''' [rar file] [alphabet]
+Examples: '''+name+''' foobar.rar
+          '''+name+''' foobar.rar abc\~\(d1234567890\-'''
+
 #checking if the file exists/running the function
-if len(sys.argv)==2:
- if os.path.exists(sys.argv[1]):
-  rc(sys.argv[1])
- else:
-  print("ERROR: File doesn't exist.\nExiting...")
+argLen= len(sys.argv)
+if argLen >= 2:
+  if not os.path.exists(sys.argv[1]):
+    print("ERROR: File doesn't exist.\nExiting...")
+  else:
+    if argLen ==3:
+      rc(sys.argv[1], sys.argv[2])
+    elif argLen == 2:
+      print ('Using default alphabet: '+defaultAlphabet)
+      rc(sys.argv[1], defaultAlphabet )
+    else:
+      print(helpMessage)
 else:
- print("Usage:",name,"[rar file]")
- print("Example:",name,"foobar.rar")
+  print(helpMessage)
